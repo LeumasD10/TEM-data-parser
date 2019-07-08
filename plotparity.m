3831d362526d6188f30018308a45800c4093d83c
Samuel Ma <yeshun@shaw.ca>
2019-07-07 21:38:01 -0700

    Made slopes a struct to allow further analysis

--- plotparity.m
+++ plotparity.m
@@ -22,24 +22,28 @@ hold on;
 errorbar(man.dp,pcm.sim,man.dev,man.dev, ...
     'Horizontal','r.','MarkerSize',20);
 
-% Compute line of best fit starting at origin
-% y = ax, a = x\y to solve linear system
-a = man.dp'\pcm.sim';
-% Concatenate origin to best fit data set
-fitx = [0 man.dp];
-fity = [0 a*man.dp];
-plot(fitx,fity,'r');
-
 % Plot Generalized PCM
 errorbar(man.dp,pcm.gen,man.dev,man.dev, ...
     'Horizontal','k.','MarkerSize',20);
 
 % Compute line of best fit starting at origin
 % y = ax, a = x\y to solve linear system
-a = man.dp'\pcm.gen';
+
+a = struct;
+
+a.pcmsim = man.dp'\pcm.sim';
 % Concatenate origin to best fit data set
 fitx = [0 man.dp];
-fity = [0 a*man.dp];
+fity = [0 a.pcmsim*man.dp];
+plot(fitx,fity,'r');
+
+
+% Compute line of best fit starting at origin
+% y = ax, a = x\y to solve linear system
+a.pcmgen = man.dp'\pcm.gen';
+% Concatenate origin to best fit data set
+fitx = [0 man.dp];
+fity = [0 a.pcmgen*man.dp];
 plot(fitx,fity,'k');
 
 % Plot a 1:1 relation
@@ -52,8 +56,8 @@ xlabel('Manual dp (nm)');
 ylabel('Method dp (nm)');
 title('Parity Plot of Select Method dp Against Manual dp Sizing');
 legend({'PCM Simplified Data and STDEV', ...
-    'PCM Simplified Best Fit Line', ... 
     'PCM General Data and STDEV', ...
+    'PCM Simplified Best Fit Line', ...
     'PCM General Best Fit Line' ...
     '1:1 Idealized Reference'}, ... 
     'Location','southeast');
